25b9c9ffa9169960dac97b2e69d8f191c50fd3b5
Ben Parr <bparr@bparr.com>
2018-02-03 15:43:43 -0500

    Introduce and use _GameStateAdvancer.

--- melee/example.py
+++ melee/example.py
@@ -13,13 +13,95 @@ def check_port(value):
 
 
 # TODO ensure maintain four space tabs.
+# TODO ensure opponent --> opponent_port.
+# TODO note that currently only works as a singleton. Or just rename to
+#      getGameFrameAdvancer and hand code memoization. Make sure to store.
 def newGameFrameAdvancer(port, opponent_port, iso_path):
     port = check_port(port)
     opponent_port = check_port(opponent_port)
-    # TODO finish implementing.
+    opponent_type = melee.enums.ControllerType.STANDARD
+    dolphin = melee.dolphin.Dolphin(ai_port=port, opponent_port=opponent_port,
+                                    opponent_type=opponent_type)
+    gamestate = melee.gamestate.GameState(dolphin)
+    controller = melee.controller.Controller(port=port, dolphin=dolphin)
+    opponent_controller = melee.controller.Controller(port=opponent_port,
+                                                      dolphin=dolphin)
+    def signal_handler(signal, frame):
+        dolphin.terminate()
+        print("Shutting down cleanly...")
+        sys.exit(0)
+
+    # TODO better handle multiple dolphin instances? As of now, a previous
+    #      dolphin will have its signal handler removed.
+    signal.signal(signal.SIGINT, signal_handler)
+    dolphin.run(render=True, iso_path=args.iso_path)
+    #Plug our controller in
+    #   Due to how named pipes work, this has to come AFTER running dolphin
+    controller.connect()
+    opponent_controller.connect()
+    return _GameFrameAdvancer(gamestate, dolphin, controller,
+                              opponent_controller)
 
 
-chain = None
+class _GameFrameAdvancer(object):
+    def __init__(self, gamestate, dolphin, controller, opponent_controller):
+        self._gamestate = gamestate
+        self._dolphin = dolphin
+        self._controller = controller
+        self._opponent_controller = opponent_controller
+        self._first_match = True
+
+    def step(self):
+        done_stepping = False
+        while not done_stepping:
+            done_stepping = self._step_helper()
+            self._controller.flush()
+            self._opponent_controller.flush()
+
+    def _step_helper(self):
+        gamestate = self._gamestate  # TODO inline?
+        dolphin = self._dolphin      # TODO inline?
+        gamestate.step()
+        if(gamestate.processingtime * 1000 > 12):
+            print("WARNING: Last frame took " + str(gamestate.processingtime*1000) + "ms to process.")
+
+        #What menu are we in?
+        if gamestate.menu_state in [melee.enums.Menu.IN_GAME, melee.enums.Menu.SUDDEN_DEATH]:
+            # TODO remove.
+            melee.techskill.multishine(ai_state=gamestate.ai_state, controller=self._controller)
+            return True
+
+        #If we're at the character select screen, choose our character
+        elif gamestate.menu_state == melee.enums.Menu.CHARACTER_SELECT:
+            melee.menuhelper.choosecharacter(character=melee.enums.Character.FOX,
+                                            gamestate=gamestate,
+                                            port=dolphin.ai_port,
+                                            opponent_port=dolphin.opponent_port,
+                                            controller=self._controller,
+                                            swag=False,
+                                            start=True)
+            if self._first_match:
+              # Only set up opponent on first match. Otherwise, will switch back
+              # to non-cpu player, for example.
+              melee.menuhelper.choosecharacter(character=melee.enums.Character.MARTH,
+                                              gamestate=gamestate,
+                                              port=dolphin.opponent_port,
+                                              opponent_port=dolphin.ai_port,
+                                              controller=self._opponent_controller,
+                                              make_cpu=True,
+                                              swag=False,
+                                              start=True)
+        #If we're at the postgame scores screen, spam START
+        elif gamestate.menu_state == melee.enums.Menu.POSTGAME_SCORES:
+            self._first_match = False
+            melee.menuhelper.skippostgame(controller=self._controller)
+        #If we're at the stage select screen, choose a stage
+        elif gamestate.menu_state == melee.enums.Menu.STAGE_SELECT:
+            melee.menuhelper.choosestage(stage=melee.enums.Stage.FINAL_DESTINATION,
+                                        gamestate=gamestate,
+                                        controller=self._controller)
+        return False
+
 
 parser = argparse.ArgumentParser(description='Example of libmelee in action')
 parser.add_argument('--port', '-p', type=check_port,
@@ -33,77 +115,8 @@ parser.add_argument('--iso_path', required=True,
 
 args = parser.parse_args()
 
-#Create our Dolphin object. This will be the primary object that we will interface with
-dolphin = melee.dolphin.Dolphin(ai_port=args.port,
-                                opponent_port=args.opponent,
-                                opponent_type=melee.enums.ControllerType.STANDARD)
-#Create our GameState object for the dolphin instance
-gamestate = melee.gamestate.GameState(dolphin)
-#Create our Controller object that we can press buttons on
-controller = melee.controller.Controller(port=args.port, dolphin=dolphin)
-opponent_controller = melee.controller.Controller(port=args.opponent, dolphin=dolphin)
-
-def signal_handler(signal, frame):
-    dolphin.terminate()
-    print("Shutting down cleanly...")
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
-
-#Run dolphin and render the output
-dolphin.run(render=True, iso_path=args.iso_path)
-
-#Plug our controller in
-#   Due to how named pipes work, this has to come AFTER running dolphin
-#   NOTE: If you're loading a movie file, don't connect the controller,
-#   dolphin will hang waiting for input and never receive it
-controller.connect()
-opponent_controller.connect()
-first_match = True
-
-#Main loop
+game_state_advancer = newGameFrameAdvancer(args.port, args.opponent,
+                                           args.iso_path)
 while True:
-    #"step" to the next frame
-    gamestate.step()
-    if(gamestate.processingtime * 1000 > 12):
-        print("WARNING: Last frame took " + str(gamestate.processingtime*1000) + "ms to process.")
-
-    #What menu are we in?
-    if gamestate.menu_state in [melee.enums.Menu.IN_GAME, melee.enums.Menu.SUDDEN_DEATH]:
-        #XXX: This is where your AI does all of its stuff!
-        #This line will get hit once per frame, so here is where you read
-        #   in the gamestate and decide what buttons to push on the controller
-        melee.techskill.multishine(ai_state=gamestate.ai_state, controller=controller)
-    #If we're at the character select screen, choose our character
-    elif gamestate.menu_state == melee.enums.Menu.CHARACTER_SELECT:
-        melee.menuhelper.choosecharacter(character=melee.enums.Character.FOX,
-                                        gamestate=gamestate,
-                                        port=args.port,
-                                        opponent_port=args.opponent,
-                                        controller=controller,
-                                        swag=False,
-                                        start=True)
-        if first_match:
-          # Only set up opponent on first match. Otherwise, will switch back
-          # to non-cpu player, for example.
-          melee.menuhelper.choosecharacter(character=melee.enums.Character.MARTH,
-                                          gamestate=gamestate,
-                                          port=args.opponent,
-                                          opponent_port=args.port,
-                                          controller=opponent_controller,
-                                          make_cpu=True,
-                                          swag=False,
-                                          start=True)
-    #If we're at the postgame scores screen, spam START
-    elif gamestate.menu_state == melee.enums.Menu.POSTGAME_SCORES:
-        first_match = False
-        melee.menuhelper.skippostgame(controller=controller)
-    #If we're at the stage select screen, choose a stage
-    elif gamestate.menu_state == melee.enums.Menu.STAGE_SELECT:
-        melee.menuhelper.choosestage(stage=melee.enums.Stage.FINAL_DESTINATION,
-                                    gamestate=gamestate,
-                                    controller=controller)
-    #Flush any button presses queued up
-    controller.flush()
-    opponent_controller.flush()
+    game_state_advancer.step()
 
